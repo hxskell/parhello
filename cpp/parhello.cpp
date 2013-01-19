392794b5574f4bd2b93abff31d536754e1aa119e
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-19 02:48:28 -0500

    C++

---
+++ cpp/parhello.cpp
@@ -0,0 +1,16 @@
+#include <omp.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s = "Hello World!\n";
+	int i;
+
+	# pragma omp parallel for
+	for (i = 0; i < s.length(); i++) {
+		cout << s[i];
+	}
+
+	return 0;
+}
