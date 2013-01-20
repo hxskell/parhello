5c74ab1b41f33483546f7bde8f070af072141c87
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-20 11:40:48 -0500

    resolving Python errors

--- haskell/parhello.hs
+++ haskell/parhello.hs
@@ -17,5 +17,6 @@ main = do
 	-- By default, buffering prevents some IO nondeterminism
 	hSetBuffering stdout NoBuffering
 
-	parallel_ (map putChar "Hello World!")
+	parallel_ $ map putChar "Hello World!"
+
 	stopGlobalPool
--- python/parhello.py
+++ python/parhello.py
@@ -3,14 +3,11 @@
 import pp
 import sys
 
-def hello(c):
-	sys.stdout.write(c)
-
 def main():
 	server = pp.Server()
 
 	for c in "Hello World!\n":
-		server.submit(hello, (c,), (), ("sys",))()
+		server.submit(sys.stdout.write, (c,), (), ("sys",))()
 
 if __name__=="__main__":
 	main()
