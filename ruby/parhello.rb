c3abe827194e79aaf1c2290bbba4c8c2806e8de5
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-20 12:49:29 -0500

    1.8.7

--- ruby/parhello.rb
+++ ruby/parhello.rb
@@ -3,8 +3,4 @@
 require "rubygems"
 require "parallel"
 
-def main
-	Parallel.map("Hello World!\n".split("")) { |c| putc c }
-end
-
-main if __FILE__ == $0
+Parallel.map("Hello World!\n".split("")) { |c| putc c }
