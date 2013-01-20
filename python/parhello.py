b5f1b72f9470cc4e2a6c4a09653964e470ffec0a
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-20 11:54:46 -0500

    resolving Ruby errors

--- python/parhello.py
+++ python/parhello.py
@@ -3,11 +3,14 @@
 import pp
 import sys
 
+def hello(c):
+	sys.stdout.write(c)
+
 def main():
 	server = pp.Server()
 
 	for c in "Hello World!\n":
-		server.submit(sys.stdout.write, (c,), (), ("sys",))()
+		server.submit(hello, (c,), (), ("sys",))()
 
 if __name__=="__main__":
 	main()
