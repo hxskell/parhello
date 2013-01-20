389f8cb81d51c63d0342288965e88cdcc6343605
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-20 16:18:58 -0500

    cleaning

--- erlang/parhello.erl
+++ erlang/parhello.erl
@@ -1,11 +1,11 @@
 -module(parhello).
 -author("andrew.pennebaker@gmail.com").
--export([hello/1, main/1]).
+-export([hello/1, main/0]).
 
 -import(lists, [map/2]).
 
 hello(C) ->
 	io:format("~c", [C]).
 
-main(_) ->
+main() ->
 	lists:map(fun(C) -> spawn(parhello, hello, [C]) end, "Hello World!\n").
