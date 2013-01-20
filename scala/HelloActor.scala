4a72bc72d1a048b1923a6407548116b10406d19b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-20 14:15:35 -0500

    resolving Scala errors

---
+++ scala/HelloActor.scala
@@ -0,0 +1,31 @@
+// Parallel Hello World using actors
+
+import scala.actors.Actor
+import scala.actors.Actor._
+
+case class Store(v : Char)
+case class Show()
+
+object HActor extends Actor {
+	var c : Char = ' ';
+
+	def act() {
+		while (true) {
+			receive {
+				case Store(v) => c = v
+				case Show() => print(c)
+			}
+		}
+	}
+
+	def main(args : Array[String]) {
+		val s = "Hello World!"
+
+		val as : Array[HelloActor] = new Array[HelloActor](s.length)
+
+		for (i <- 0 until s.length() - 1) {
+			as(i) ! Store(s(i))
+			as(i) ! Show()
+		}
+	}
+}
