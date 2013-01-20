7ba589f010e29407c9f8661c4722fe6a3f9cef0f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-20 14:28:31 -0500

    NullPointerException

--- scala/HelloActor.scala
+++ scala/HelloActor.scala
@@ -6,7 +6,7 @@ import scala.actors.Actor._
 case class Store(v : Char)
 case class Show()
 
-object HelloActor extends Actor {
+class HActor extends Actor {
 	var c : Char = ' ';
 
 	def act() = {
@@ -17,15 +17,15 @@ object HelloActor extends Actor {
 			}
 		}
 	}
+}
 
+object HelloActor {
 	def main(args : Array[String]) {
 		val s = "Hello World!"
 
-		val as : Array[HelloActor] = new Array[HelloActor](s.length)
+		val as : Array[HActor] = new Array[HActor](s.length)
 
-		for (i <- 0 until s.length() - 1) {
-			as(i) ! Store(s(i))
-			as(i) ! Show()
-		}
+		(as, s).zipped map (_ ! Store(_))
+		as.map (_ ! Show())
 	}
 }
