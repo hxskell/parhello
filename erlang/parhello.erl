ce734116afb6cca766822e78f39ca22b9615b179
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-02-13 18:33:57 -0500

    idioms

--- erlang/parhello.erl
+++ erlang/parhello.erl
@@ -1,11 +1,16 @@
 -module(parhello).
 -author("andrew.pennebaker@gmail.com").
--export([hello/1, start/0]).
+-export([hello/0, start/0]).
 
 -import(lists, [map/2]).
 
-hello(C) ->
-	io:format("~c", [C]).
+hello() ->
+	receive
+		C -> io:format("~c", [C])
+	end.
 
 start() ->
-	lists:map(fun(C) -> spawn(parhello, hello, [C]) end, "Hello World!\n").
+	lists:map(
+		fun(C) -> spawn(parhello, hello, []) ! C end,
+		"Hello World!\n"
+	).
