fd4c9ecb7417280df789e3edcca34ccd8a1ac05f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-03-21 18:22:56 -0400

    split openmp vs c++11

---
+++ cpp/parhello-c++11/parhello-c++11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <future>
+using namespace std;
+
+int main() {
+  string message = "Hello World!\n";
+
+  vector<int> range(message.length(), 0);
+  iota(range.begin(), range.end(), 0);
+
+  for_each(range.begin(), range.end(), [&](int i) {
+      std::async(
+                 launch::async,
+                 [&]() {
+                   cout << message[i];
+                   cout.flush();
+                 }
+                 );
+    });
+
+  return 0;
+}
