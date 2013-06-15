58b030b8d6299dfabc8fe3245a9260bb3a39cdf8
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-06-15 16:08:25 -0400

    debugging cloud haskell

--- haskell/parhello-actors/parhello.hs
+++ haskell/parhello-actors/parhello.hs
@@ -1,64 +1,37 @@
 #!/usr/bin/env runhaskell
 
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import Control.Distributed.Process
 import Control.Distributed.Process.Node (initRemoteTable, runProcess)
-import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Distributed.Process.Backend.SimpleLocalnet (Backend, startMaster, initializeBackend, newLocalNode, terminateAllSlaves)
 
-import Data.Maybe
+import Data.Binary (Binary)
+import Data.Typeable (Typeable)
+
 import System.Environment (getArgs)
 
-data ImTheCounter = ImTheCounter ProcessId
-data ImTheMaster = ImTheMaster ProcessId
-data Say = Say Char
-data Done = Done
-data Dec = Dec
+newtype Speaker = Speaker ProcessId deriving (Typeable, Binary)
+newtype Say = Say Char deriving (Typeable, Binary)
 
-counter :: (Maybe ProcessId) -> Int -> [NodeId] -> Process ()
-counter Nothing n _ = receiveWait [
-  match (\(ImTheMaster masterPid') -> do
-    counterPid <- getSelfPid
-    send masterPid' (ImTheCounter counterPid)
-    counter (Just masterPid') n [])
-  ]
-counter (Just masterPid') 0 _ = send masterPid' Done
-counter masterPid n _ = receiveWait [
-  match (\Dec -> counter masterPid (n-1) [])
-  ]
-
-speaker :: ProcessId -> [NodeId] -> Process ()
-speaker counterPid _ = do
+speaker :: Process ()
+speaker = do
+  speakerPid <- getSelfPid
+  nsend "master" (Speaker speakerPid)
   receiveWait [
     match (\(Say c) -> do
-      say (c:[])
-      send counterPid Dec
-      speaker counterPid [])
+              say (c:[])
+              speaker)
     ]
 
-message :: String
-message = "Hello World!"
-
-done :: Int
-done = length message
-
-master :: Backend -> [NodeId] -> Process ()
-master backend workers = do
-  masterPid <- getSelfPid
-
-  -- Find the counter
-  mapM (\worker -> send worker (ImTheMaster masterPid)) workers
-  receiveWait [
-    match (\(ImTheCounter counterPid) -> do
-       -- Inform the speakers
-       let speakers = workers - [counterPid]
-       mapM (\(speaker,c) -> send speaker c) $ zip (cycle speakers) message
-
-       -- Wait for count
-       receiveWait [
-         match (\Done -> terminateAllSlaves backend)
-         ])
-    ]
+master :: String -> Backend -> [NodeId] -> Process ()
+master "" backend _ = terminateAllSlaves backend
+master (c:cs) backend _ = receiveWait [
+  match (\(Speaker s) -> do
+            send s (Say c)
+            master cs backend [])
+  ]
 
 main :: IO ()
 main = do
@@ -67,12 +40,9 @@ main = do
   case args of
     ["master", host, port] -> do
       backend <- initializeBackend host port initRemoteTable
-      startMaster backend (master backend)
+      let message = "Hello World!"
+      startMaster backend (master message backend)
     ["speaker", host, port] -> do
       backend <- initializeBackend host port initRemoteTable
       node <- newLocalNode backend
       runProcess node speaker
-    ["counter", host, port] -> do
-      backend <- initializeBackend host port initRemoteTable
-      node <- newLocalNode backend
-      runProcess node (counter Nothing done [])
