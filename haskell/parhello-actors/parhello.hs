35a9023fdee0c411e6d2b8f14cc2f3791e4facbd
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-06-15 17:28:55 -0400

    cloud haskell clean

--- haskell/parhello-actors/parhello.hs
+++ haskell/parhello-actors/parhello.hs
@@ -19,8 +19,6 @@ newtype Say = Say Char deriving (Typeable, Binary)
 
 speaker :: Maybe ProcessId -> Process ()
 speaker Nothing = do
-  say "waiting for master to connect"
-
   masterPid <- whereis "master"
 
   case masterPid of
@@ -30,54 +28,27 @@ speaker Nothing = do
       speaker Nothing
 
 speaker (Just masterPid') = do
-  say "waiting for instructions for master"
-
   speakerPid <- getSelfPid
-
   send masterPid' (Speaker speakerPid)
-
   receiveWait [
     match (\(Say c) -> do
-              say "got work"
-
               say (c:[])
               speaker (Just masterPid'))
     ]
 
 master :: Bool -> String -> Backend -> [NodeId] -> Process ()
 master False message backend _ = do
-  say "registering"
-
   masterPid' <- getSelfPid
   register "master" masterPid'
 
   peers <- liftIO $ findPeers backend 20000 -- ms
-
-  say $ "peers: " ++ show peers
-
-  mapM (\peer -> do
-           say $ "registering with peer " ++ show peer
-           registerRemoteAsync peer "master" masterPid'
-       ) peers
-
-  -- receiveWait [
-  --   match (\(RegisterReply s b) -> do
-  --             say s
-  --             say (show b))
-  --   ]
-
+  mapM (\peer -> registerRemoteAsync peer "master" masterPid') peers
 
   master True message backend []
 master True "" backend _ = terminateAllSlaves backend
 master True (c:cs) backend _ = do
-  say "master loop"
-
-  say "waiting for workers to request work"
-
   receiveWait [
     match (\(Speaker s) -> do
-              say "speaker requesting work"
-
               send s (Say c)
               master True cs backend [])
     ]
