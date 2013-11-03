b227414826a52f9ab215a13207120d70a9d1f3c5
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-03 15:33:39 -0500

    added ObjC, with Grand Central Dispatch / libdispatch

---
+++ objc/parhello.m
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+
+int main() {
+  NSString* message = @"Hello World!";
+
+  dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  dispatch_group_t group = dispatch_group_create();
+
+  for (NSUInteger i = 0; i < [message length]; i++) {
+    dispatch_group_async(group, queue, ^{
+      NSLog(@"%c", [message characterAtIndex:i]);
+    });
+  }
+
+  dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+  dispatch_release(group);
+
+  return 0;
+}
