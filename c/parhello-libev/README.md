bef072d55c1f0ca0d2bd9df6062c334d08bfd21f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-13 21:24:48 -0500

    guard: removed bundler check

---
+++ c/parhello-libev/README.md
@@ -0,0 +1,5 @@
+# REQUIREMENTS
+
+* [libev](http://software.schmorp.de/pkg/libev.html)
+
+E.g., `brew install libev`.
---
+++ c/parhello-libev/parhello.c
@@ -0,0 +1,34 @@
+#include <ev.h>
+#include <stdio.h>
+#include <string.h>
+
+static ev_async signature;
+
+void printer(struct ev_loop *loop, struct ev_io *watcher, int revents) {
+  char data;
+
+  while (queue_get(&data)) {
+    putc(watcher->c);
+  }
+}
+
+int main() {
+  char *s = "Hello World!\n";
+
+  size_t i;
+
+  struct ev_loop *loop = ev_default_loop(0);
+  struct ev_io watcher;
+
+  ev_io_init(&watcher, printer, 0, EV_READ);
+  ev_io_start(loop, &watcher);
+
+  ev_run(loop, 0);
+
+  for (i = 0; i < s; i++) {
+    queue_put(s[i]);
+    ev_async_send(&signature);
+  }
+
+  return 0;
+}
