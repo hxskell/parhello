e6aec447b53085c38fb3b5707103afff2fa50371
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-12-12 18:57:13 -0500

    checkstyle

--- java/ParHello.java
+++ java/ParHello.java
@@ -1,18 +1,30 @@
+/** Hello World in parallel */
 public final class ParHello {
+  /** Actor */
   private static class CharPrinter implements Runnable {
     private char c;
 
+    /**
+       <p>Construct actor</p>
+       @param c character to print
+    */
     public CharPrinter(final char c) {
       this.c = c;
     }
 
+    /** Act */
     public void run() {
       System.out.print(c);
     }
   }
 
+  /** utility class */
   private ParHello() {}
 
+  /**
+     <p>Main app</p>
+     @param args CLI arguments
+  */
   public static void main(final String[] args) {
     String message = "Hello World!\n";
 
